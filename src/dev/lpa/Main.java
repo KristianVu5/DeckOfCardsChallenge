459b005adac83e2de659fa714c411692c7b6af13
Kristian Vu <krisivu5@gmail.com>
2026-09-17 22:56:44 +0300

    Collections fill,add and copy methods

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,10 +1,34 @@
 package dev.lpa;
 
-import java.util.List;
+import java.util.*;
 
 public class Main {
     static void main(String[] args) {
         List<Card> deck = Card.getStandardDeck();
         Card.printDeck(deck);
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
+        Arrays.fill(cardArray, aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray), "Ace of Hearts", 1);
+
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards,aceOfHearts);
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
+
+        List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
+
+        Card kingOfClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Card.printDeck(kingsOfClubs, "Kings of Clubs", 1);
+
+        Collections.addAll(cards, cardArray);
+        Collections.addAll(cards, cardArray);
+        Card.printDeck(cards,"Card Collection with Aces added",1);
+
+        Collections.copy(cards, kingsOfClubs);
+        Card.printDeck(cards,"Card Collection with Kings copied",2);
     }
 }
